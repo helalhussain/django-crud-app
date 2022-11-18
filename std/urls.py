a8087d2d8b486c0486e10aad45a90266be708fbf
helal hussain <helalhussain90@gmail.com>
2022-11-18 14:19:58 +0600

    Edit

--- std/urls.py
+++ std/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from.import views
 urlpatterns = [
 
-    path('home/',views.home),
+    path('',views.home),
     path('add-std/',views.add_std),
+    path('delete-std/<int:roll>',views.delete_std),
+    path('edit-std/<int:roll>',views.update_std),
+    path('do-edit-std/<int:roll',views.do_edit),
     
 ]
--- std/views.py
+++ std/views.py
@@ -22,8 +22,32 @@ def add_std(request):
         s.address=stds_address
         s.phone=stds_phone
         s.save()
-        return redirect('/std/home')
-
-
+        return redirect('/')
 
     return render(request,'std/add_std.html', {})
+
+def delete_std(request,roll):
+    s=Student.objects.get(pk=roll)
+    s.delete()
+    return redirect('/')
+
+def update_std(request,roll):
+    std=Student.objects.get(pk=roll)
+    return render(request,'std/edit.html',{'std':std})
+
+def do_edit(request,roll):
+
+    std_roll = request.POST.get('std_roll')
+    std_name = request.POST.get('std_name')
+    std_email = request.POST.get('std_email')
+    std_address=request.POST.get('std_address')
+    std_phone = request.POST.get('std_phone')
+
+    std=Student.objects.get(pk=roll)
+    std.roll=std_roll
+    std.name=std_name
+    std.email=std_email
+    std.address=std_address
+    std.phone=std_phone
+    std.save()
+    return redirect('/')
